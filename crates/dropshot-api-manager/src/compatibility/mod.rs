86ccf54586508c9286d998c563d8272649d72df6
Rain <rain@oxide.computer>
2026-05-15 16:44:17 -0700

    [spr] changes to main this commit is based on
    Created using spr 1.3.6-beta.1
    
    [skip ci]

--- crates/dropshot-api-manager/src/compatibility/mod.rs
+++ crates/dropshot-api-manager/src/compatibility/mod.rs
@@ -3,5 +3,10 @@
 //! Determine if one OpenAPI document is a subset of another.
 
 mod detect;
+mod display;
+mod types;
 
-pub use detect::{ApiCompatIssue, api_compatible};
+pub use detect::api_compatible;
+pub(crate) use detect::{CompatDedupeMap, DedupeStatus};
+pub use types::ApiCompatIssue;
+pub(crate) use types::CompatIssueLocation;
